d618c3b6902af024b57ccf4be0c0d6b4dfb34051
Connor0728 <connorcart@gmail.com>
2014-03-23 23:24:20 +0000

    No need to run checks
    yeeee

--- JPD_Cwk_2014_Group20/src/aston/group20/model/Air_Control_Tower.java
+++ JPD_Cwk_2014_Group20/src/aston/group20/model/Air_Control_Tower.java
@@ -44,8 +44,6 @@ public class Air_Control_Tower {
 
 	public void addOutgoing(Aircraft a) {
 		outgoing.add(a);
-		System.out.println("Aircraft should be added");
-		System.out.println(outgoing.size());
 	}
 
 	public PriorityQueue<Aircraft> getOutgoing() {
