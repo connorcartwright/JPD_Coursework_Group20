a64083b6c505663b1e51736988ad5c004f7b2bc0
Sayedtm <taariq7867@hotmail.co.uk>
2014-04-06 20:03:01 +0100

    Update Unpowered_Aircraft.java

--- JPD_Cwk_2014_Group20/src/aston/group20/model/Unpowered_Aircraft.java
+++ JPD_Cwk_2014_Group20/src/aston/group20/model/Unpowered_Aircraft.java
@@ -1,5 +1,15 @@
 package aston.group20.model;
 
+/**
+* Characterizes the shared behaviours of all Unpowered aircraft
+* fulfills the inheritence of the Aircraft class
+* @see Aircraft
+*
+* @author Group_20
+* @version 1.0, March 2014
+*
+*/
+
 public class Unpowered_Aircraft extends Aircraft {
 	
 	public Unpowered_Aircraft(int landingTime, int takeoffTime) {
