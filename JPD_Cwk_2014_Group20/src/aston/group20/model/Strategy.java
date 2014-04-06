e2080c3c615aa3564670f9af95c78abd71ccedb2
Sayedtm <taariq7867@hotmail.co.uk>
2014-04-06 19:52:47 +0100

    Update Strategy.java

--- JPD_Cwk_2014_Group20/src/aston/group20/model/Strategy.java
+++ JPD_Cwk_2014_Group20/src/aston/group20/model/Strategy.java
@@ -1,6 +1,15 @@
 package aston.group20.model;
 import java.util.Comparator;
 
+/**
+* The Strategy class is used to define how aircrafts should be sorted 
+* as well as how they should land and take off 
+*
+* @author Group_20
+* @version 1.0, March 2014
+*
+*/
+
 public abstract class Strategy implements Comparator<Object> {
 
 	public abstract int compare(Object o1, Object o2);
