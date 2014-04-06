10e6cf662144db69948bd062c755a27e698acab9
Sayedtm <taariq7867@hotmail.co.uk>
2014-04-06 19:14:19 +0100

    Update Glider.java

--- JPD_Cwk_2014_Group20/src/aston/group20/model/Glider.java
+++ JPD_Cwk_2014_Group20/src/aston/group20/model/Glider.java
@@ -1,5 +1,15 @@
 package aston.group20.model;
 
+/**
+* The Glider class that sets the times for both landing and taking off 
+* fulfills the inheritence of UnpoweredAircraft 
+* @see Unpowered_Aicraft
+*
+* @author Group_20
+* @version 1.0, March 2014
+*
+*/
+
 public class Glider extends Unpowered_Aircraft {
 	
 	// The amount of time it takes to take off
